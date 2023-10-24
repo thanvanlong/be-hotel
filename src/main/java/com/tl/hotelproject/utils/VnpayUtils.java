55adc823e578740671596457cb8c04ca395a4bb6
ducnm <ngoduc2468@gmail.com>
2023-10-24 23:49:00 +0700

    fix vnpay

--- src/main/java/com/tl/hotelproject/utils/VnpayUtils.java
+++ src/main/java/com/tl/hotelproject/utils/VnpayUtils.java
@@ -4,6 +4,8 @@ import com.tl.hotelproject.dtos.bills.VnpayCreatePaymentDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -203,17 +205,25 @@ public class VnpayUtils {
         }
     }
 
-    private String hmacSHA512(String secret, String data) {
+    public String hmacSHA512(String key, final String data) {
         try {
-            MessageDigest md = MessageDigest.getInstance("SHA-512");
-            byte[] bytes = md.digest(data.getBytes());
-            StringBuilder sb = new StringBuilder();
-            for (byte aByte : bytes) {
-                sb.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
+
+            if (key == null || data == null) {
+                throw new NullPointerException();
+            }
+            final Mac hmac512 = Mac.getInstance("HmacSHA512");
+            byte[] hmacKeyBytes = key.getBytes();
+            final SecretKeySpec secretKey = new SecretKeySpec(hmacKeyBytes, "HmacSHA512");
+            hmac512.init(secretKey);
+            byte[] dataBytes = data.getBytes(StandardCharsets.UTF_8);
+            byte[] result = hmac512.doFinal(dataBytes);
+            StringBuilder sb = new StringBuilder(2 * result.length);
+            for (byte b : result) {
+                sb.append(String.format("%02x", b & 0xff));
             }
             return sb.toString();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+
+        } catch (Exception ex) {
             return "";
         }
     }
